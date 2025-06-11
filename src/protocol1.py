fd3f247a526f32dadf8db48b4de2ea2850d4e677
AndBoyS <andboyxd@gmail.com>
2025-06-11 10:53:30 +0300

    adjust examples

--- src/protocol1.py
+++ src/protocol1.py
@@ -2,12 +2,13 @@ from typing import Protocol
 
 
 class NotifyImplementation(Protocol):
-    def __call__(self, to: str, subject: str, body: str) -> None:
+    def notify(self, to: str, subject: str, body: str) -> None:
         pass
 
 
-def send_email(to: str, subject: str, body: str) -> None:
-    print(f"Sending email to {to}: {subject} - {body}")
+class NotifyEmail:
+    def notify(self, to: str, subject: str, body: str) -> None:
+        print(f"Sending email to {to}: {subject} - {body}")
 
 
 class NotificationManager:
@@ -15,8 +16,8 @@ class NotificationManager:
         self.notify_impl = notify_impl
 
     def notify(self, user_email: str, message: str) -> None:
-        self.notify_impl(user_email, "Notification", message)
+        self.notify_impl.notify(user_email, "Notification", message)
 
 
-email_notifier = NotificationManager(send_email)
+email_notifier = NotificationManager(NotifyEmail())
 email_notifier.notify("user@example.com", "Hello from Dependency Injection")
